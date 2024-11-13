d82a171cd19be4078811b156e5dcccc84dfcce5d
Iryna Bohomiakova <irinkavakula@gmail.com>
2024-11-13 01:49:51 +0100

    First Brunch

--- HomeWork/src/test/java/es/santander/ascender/ArrayListTest.java
+++ HomeWork/src/test/java/es/santander/ascender/ArrayListTest.java
@@ -1,5 +1,24 @@
 package es.santander.ascender;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class ArrayListTest {
+      
+    
+    
+    @Test
+    public void shouldAnswerWithTrue() {
+        int[][] mas = new int[5][5];
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {5, 6, 7, 8, 9};
+        int result = double.(mas);
+
+        assertEquals(result, 5)
+
+        
+    }
 
 }
